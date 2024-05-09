6882c8a5625406bd41bd07fed975b28758af42d4
Kael Foxwell <kaefoxewe@gmail.com>
2024-05-09 13:43:55 +1000

    Create InvoiceFactory.cs

---
+++ KaelsToolBox_2/Web/Database/Invoicing/InvoiceFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaelsToolBox_2.Web.Database.Invoicing;
+
+public class InvoiceFactory(Client client, Provider provider, BillableItem[] items) 
+{
+    public Invoice Output { get; private set; } = new();
+
+    private Client _Client = client;
+    private Provider _Provider = provider;
+    private BillableItem[] _Items = items;
+
+
+}
