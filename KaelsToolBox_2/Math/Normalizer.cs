fc8d365f5d193a661e471dce831c0fc750b8722e
Kael Foxwell <kaefoxewe@gmail.com>
2024-06-14 18:46:58 +1000

    .

---
+++ KaelsToolBox_2/Math/Normalizer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaelsToolBox_2.Math;
+
+public static class Normalizer
+{
+    public static float[] Floats(float[] input)
+    {
+        // if input size is 0 or 1 we do not need to do anything
+        if (input.Length <= 1) return input;
+
+        float[] newValues = new float[input.Length];
+
+        // get total value
+        float total = 0f;
+        foreach (var item in input) total += item;
+
+        // divide everything by total value
+        for (int i = 0; i < input.Length; i++) newValues[i] = input[i] / total;
+
+        // return the normalized array
+        return newValues;
+    }
+}
